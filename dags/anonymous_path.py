24519e1b39570746c6d8dc805ab30452a77f9f7f
MokaMoka77 <tbuiquang103@gmail.com>
2025-12-11 22:44:00 +0700

    update git anonymous_path

--- dags/anonymous_path.py
+++ dags/anonymous_path.py
@@ -1,9 +1,11 @@
+import os
+
 class Path_Folder:
-    base_dir = "/usr/local/airflow/data"
+    base_dir = "/usr/local/airflow/include/data"
 
     processed_folder_path = f"{base_dir}/processed"
     raw_folder_path = f"{base_dir}/raw"
 
     def __init__(self):
-        self.processed_folder_path = Path_Folder.processed_folder_path
-        self.raw_folder_path = Path_Folder.raw_folder_path
+        os.makedirs(self.processed_folder_path, exist_ok=True)
+        os.makedirs(self.raw_folder_path, exist_ok=True)
